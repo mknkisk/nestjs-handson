27dfce6ce33c2ddebc5c3ce506ca4f8b65cbbe7b
mknkisk <mknkisk@gmail.com>
2024-12-23 20:52:01 +0900

    Connect to the database and create tables
    npm run typeorm:generate-migration --name=CreateUsers
    npm run typeorm:run-migrations
    npm i --save class-validator class-transformer

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from '../config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        entities: ['dist/**/entities/**/*.entity.js'],
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
